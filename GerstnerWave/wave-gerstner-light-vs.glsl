daed26e798f4b9e556e6fd867cbd10f125031ef9
John Hany <johnhany@163.com>
2014-02-15 16:11:33 +0800

    adding animated tiles

--- GerstnerWave/wave-gerstner-light-vs.glsl
+++ GerstnerWave/wave-gerstner-light-vs.glsl
@@ -7,6 +7,8 @@ uniform mat4 modelViewMat;
 uniform mat4 perspProjMat;
 uniform mat3 normalMat;
 
+uniform float time;
+
 varying vec2 texture_coord;
 
 varying vec3 normalVect;
@@ -19,10 +21,12 @@ void main()
 {
     gl_Position = perspProjMat * modelViewMat * vec4(position, 1.0);
 
-	texture_coord = vec2(position.x/6.0+0.5, 0.5-position.y/6.0);
+	float tex_x = (position.x + time/20.0) / 8.0 + 0.5;
+	float tex_y = 0.5 - (position.y + time/25.0) / 5.0;
+	texture_coord = vec2(tex_x, tex_y);
 
 	vec3 eyePos = vec3(0.0, 0.0, 5.0);
-	vec3 lightPos = vec3(1.0, -5.0, 0.0);
+	vec3 lightPos = vec3(1.0, -5.0, 1.0);
 	vec3 ptVertex = vec3(modelViewMat * vec4(position, 1.0));
 
 	eyeVect = normalize(eyePos - ptVertex);
